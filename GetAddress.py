9f25da2fcf4030fe1806fefb5da80871939f2f06
brandonbracho <68869715+brandonbracho@users.noreply.github.com>
2020-08-24 19:45:21 -0400

    Create GetAddress.py
    Obtain addresses of potential spots in an area through yelp API and return them formatted to pass through popular times

---
+++ GetAddress.py
@@ -0,0 +1,37 @@
+#url https://api.yelp.com/v3/businesses/search
+import requests 
+api_key='gu4yF1nV3aomwzYAKYWMQ7H0mg-1ZpixLO-XgsvESWQqCIfSG2hUZIwEYLx42DzxPnEGdOS7t-qV0biZ3brXyObKdeu4o78tBINCgq2JuqE-MYiKY6mJ03ltQwY7X3Yx'
+endpoint= 'https://api.yelp.com/v3/businesses/search'
+headers= {'Authorization': 'bearer %s' %api_key}
+
+term=str(input('Type of nightlife experience: '))
+location=str(input('What is the address of the venue? '))
+
+parameters={'term':term,
+            'location':location,
+            'categories': 'bars,clubs,nightlife'
+           }  
+
+response= requests.get(url=endpoint,params=parameters,headers=headers)
+
+search_results=response.json()
+spots=search_results['businesses']
+formatted_address=[]
+for spot in spots:
+    name=spot['name']
+    for key,value in spot['location'].items():
+         if key=='display_address':
+                if len(value)==2:
+                    new=value[0]+ ', ' + value[1]
+                else:
+                    new=value[0]+', ' + value[1]+ ' ' + value[2]
+                together=f'({name}), {new}'
+            
+                formatted_address.append(together)
+print(formatted_address)
+            
+            
+          
+
+
+
